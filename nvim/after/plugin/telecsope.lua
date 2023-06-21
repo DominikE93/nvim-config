6e91530be3912bbf9d179e3b4be5f56084888572
dominik.lennart.eberlein <dominik.lennart.eberlein@hacon.de>
2023-06-21 12:45:56 +0200

    feat(telescope): add new keybind

--- nvim/after/plugin/telecsope.lua
+++ nvim/after/plugin/telecsope.lua
@@ -3,6 +3,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>fg', function()
-  builtin.grep_string { search = vim.fn.input 'Grep > ' }
-end)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
