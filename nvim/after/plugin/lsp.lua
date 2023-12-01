7a01938d02105ee1704d9bfbc16f5e58eb3acf9a
Dominik Schorn <dominik.schorn@nordlb.de>
2023-12-01 08:32:59 +0100

    feat: add new format & keybinds

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -90,5 +90,6 @@ null_ls.setup {
     null_ls.builtins.diagnostics.shellcheck,
     -- null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier
   },
 }
--- nvim/after/plugin/telecsope.lua
+++ nvim/after/plugin/telecsope.lua
@@ -1,8 +1,12 @@
 local builtin = require 'telescope.builtin'
 require'telescope'.load_extension'ghq'
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'find files'})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'find git files'})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'list branches'})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'list commits'})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'live grep'})
 vim.keymap.set('n', '<leader>gr', '<cmd>Telescope ghq list<cr>', { desc = 'repository' })
+vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'list buffer'})
+vim.keymap.set('n', '<leader>ll', builtin.diagnostics, { desc = 'list diagnostics'})
+vim.keymap.set('n', '<leader>cp', builtin.registers, { desc = 'registers'})
+vim.keymap.set('n', '<leader>fw', builtin.current_buffer_fuzzy_find, { desc = 'fuzzy find buffer'})
