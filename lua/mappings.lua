56da0903a6e75581628ba1680e461ddc97b28f47
Dominik Schorn <dominik.schorn@nordlb.de>
2024-04-29 07:49:33 +0200

    fix: add missing mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,19 @@
-require "nvchad.mappings"
+require 'nvchad.mappings'
 
 -- add yours here
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('i', 'jk', '<ESC>')
 
-map({ "n", "v" }, "<leader>y", '"+y', { desc = "yank to clipboard" })
-map("v", "<c-C>", '"+y', { desc = "yank to clipboard" })
-map("n", "<leader>p", '"+p', { desc = "paste from clipboard below" })
-map("n", "<leader>P", '"+P', { desc = "paste from clipboard abow" })
-map("v", "<leader>p", '"+p', { desc = "paste from clipboard" })
+map({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'yank to clipboard' })
+map('v', '<c-C>', '"+y', { desc = 'yank to clipboard' })
+map('n', '<leader>p', '"+p', { desc = 'paste from clipboard below' })
+map('n', '<leader>P', '"+P', { desc = 'paste from clipboard abow' })
+map('v', '<leader>p', '"+p', { desc = 'paste from clipboard' })
+
+-- Telescope
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>fc', builtin.command_history, { desc = 'nvim command history' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'find git files' })
